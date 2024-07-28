a775495a40240a3f35aa338db1927d578842dfad
Fscarmon <156925521+Fscarmon@users.noreply.github.com>
2024-07-29 01:53:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,25 +18,32 @@ mkdir -p download
 cd download
 
 # 定义版本和平台
-PLATFORMS=("linux-amd64" "linux-arm64" "freebsd-amd64" "linux_amd64" "linux_arm64" "freebsd_amd64")
-
+PLATFORMS=("linux-amd64" "linux-arm64" "freebsd-amd64")
+PLATFORMS2=("linux_amd64" "linux_arm64" "freebsd_amd64")
 # 下载并解压哪吒面板和客户端
 for platform in "${PLATFORMS[@]}"; do
     echo "Processing Nezha for $platform..."
 
     wget -q -O "nezha-panel-$platform.zip" "https://github.com/naiba/nezha/releases/latest/download/dashboard-$platform.zip"
-    wget -q -O "nezha-agent-$platform.zip" "https://github.com/m/nezhahq/agent/releases/latest/download/nezha-agent_$platform.zip"
-    
+       
     # 解压缩并处理 dist 文件夹中的文件
     unzip -o "nezha-panel-$platform.zip" -d "nezha-panel-$platform"
-    mv "./nezha-panel-$platform/dist/dashboard-$platform" "dashboard-$platform"
+    mv "./nezha-panel-$platform/dist/dashboard-$platform" "./dashboard-$platform"
     rm -rf "./nezha-panel-$platform"
     rm -rf  "nezha-panel-$platform.zip"
+    
+done
+for platform in "${PLATFORMS2[@]}"; do
+    echo "Processing Nezha for $platform..."
+
+    wget -q -O "nezha-agent-$platform.zip" "https://github.com/nezhahq/agent/releases/latest/download/nezha-agent_$platform.zip"
+    
+    # 解压缩并处理 dist 文件夹中的文件
     unzip -o "nezha-agent-$platform.zip" -d "nezha-agent-$platform"
     mv "./nezha-panel-$platform/nezha-agent-$platform" "nezha-agent-$platform"
     rm "nezha-agent-$platform.zip"
+    rm -rf "./nezha-agent-$platform"
 done
-
 # 下载 Xray
 echo "Downloading Xray..."
 XRAY_VERSION=$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep tag_name | cut -d '"' -f 4)
